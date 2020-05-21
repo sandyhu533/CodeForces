dbdb36fe90373db804f74a6d3f803a7c1c1bffb2
hushanshan.sandy <hushanshan.sandy@bytedance.com>
2020-05-21 10:10:56 +0800

    5.21m2

--- src/Solution.java
+++ src/Solution.java
@@ -1,64 +1,180 @@
+import java.io.*;
 import java.util.*;
-import java.math.*;
 
-// 1.LinkedList本质上是链表，查找耗时O(N)，插入、删除在预先查找的情况下也是O(N)
-// 2.树状数组，存储每个元素的数量 (1)查找到相应的位置 通过前缀和找到元素坐标 + 执行删除操作 （logn * logn + logn) (2)执行插入操作
+// 刚开始想到构成点和边，在此基础上做dijistra，但实际上边的权重是随着方案动态变化的，所以不可行
+// 用优先队列BFS，枚举每个点的不同高度，以cost少为优先进行bfs，最后问题规模将是指数级，不可行
+// 参考 https://dreamfarer.github.io/post/n4_Nmfn6u/ 所有点中必然有一个点是不动的，用O(n^2)去枚举这个不动的点
+// 再做优先队列BFS，
 
 public class Solution {
-    static int n = (int)1e6;
-    static int[] bit;
+    static class Node implements Comparable<Node>{
+        int x, y;
+        long height, cost;
 
-    static int sum(int i) {
-        int s = 0;
-        while(i > 0) {
-            s += bit[i];
-            i -= i & (-i);
+        public Node(int x, int y, long height, long cost) {
+            this.x = x;
+            this.y = y;
+            this.height = height;
+            this.cost = cost;
         }
-        return s;
-    }
 
-    static void add(int i, int val) {
-        while(i <= n) {
-            bit[i] += val;
-            i += i & (-i);
+        @Override
+        public int compareTo(Node node) {
+            return this.cost < node.cost ? -1 : 1;
         }
     }
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt();
-        int Q = scan.nextInt();
+    public static int M;
+    public static int[][] dirs = {{-1, 0}, {0, -1}};
+    public static int encode(int x, int y) {
+        return x * M + y;
+    }
 
-        bit = new int[n + 1];
-        for(int i = 0; i < N; i++) {
-            int val = scan.nextInt();
-            add(val, 1);
+    public static void main(String[] args) throws IOException {
+         Reader rd = new Reader();
+         int T = rd.nextInt();
+         for(int t = 0; t < T; t++) {
+             int n = rd.nextInt(), m = rd.nextInt();
+             long[][] height = new long[n][m];
+             for(int i = 0; i < n; i++) {
+                 for(int j = 0; j < m; j++) {
+                     height[i][j] = rd.nextLong();
+                 }
+             }
+
+             M = m;
+
+             PriorityQueue<Node> que = new PriorityQueue<>();
+             que.add(new Node(n - 1, m - 1, height[n - 1][m - 1], 0));
+
+             while(!que.isEmpty()) {
+                 Node node = que.poll();
+                 if(node.x == 0 && node.y == 0) {
+                     System.out.println(node.cost);
+                     break;
+                 }
+                 for(int[] dir : dirs) {
+                     int nx = dir[0] + node.x;
+                     int ny = dir[1] + node.y;
+                     if(nx < 0 || ny < 0)   continue;
+                     long h = height[nx][ny];
+                     long c = node.cost;
+                     if(h > node.height - 1) {
+                         c += h - node.height + 1;
+                     } else {
+                        c += node.height - h - 1;
+                     }
+                     h = node.height - 1;
+                     que.add(new Node(nx, ny, h, c));
+                 }
+             }
+
+         }
+    }
+
+    //Fast IO class
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
         }
 
-        for(int q = 0; q < Q; q++) {
-            int val = scan.nextInt();
-            if(val < 0) {
-                int l = 0, r = n;
-                while(l < r) {
-                    int mid = (l + r) >> 1;
-                    if(sum(mid) < -val) l = mid + 1;
-                    else r = mid;
+        public Reader(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n')
+                    break;
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
                 }
-                add(l, -1);
-            } else {
-                add(val, 1);
             }
+
+            if (neg)
+                return -ret;
+            return ret;
         }
 
-        if(sum(n) == 0) System.out.println(0);
-        else {
-            int l = 0, r = n;
-            while(l < r) {
-                int mid = (l + r) >> 1;
-                if(sum(mid) == 0) l = mid + 1;
-                else r = mid;
-            }
-            System.out.println(l);
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            if (din == null)
+                return;
+            din.close();
         }
     }
 }
